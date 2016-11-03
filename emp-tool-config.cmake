041354df1b2fc4761d91273ca6536f761e427408
Ubuntu <ubuntu@ip-172-31-7-191.ec2.internal>
2016-11-03 20:24:34 +0000

    update circuit file location

--- emp-tool-config.cmake
+++ emp-tool-config.cmake
@@ -6,7 +6,7 @@ find_library(EMP-TOOL_LIBRARY NAMES emp-tool)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EMP-TOOL DEFAULT_MSG EMP-TOOL_INCLUDE_DIR EMP-TOOL_LIBRARY)
 
-add_definitions(-DEMP_CIRCUIT_PATH=${EMP-TOOL_INCLUDE_DIR}/emp-tool/files)
+add_definitions(-DEMP_CIRCUIT_PATH=${EMP-TOOL_INCLUDE_DIR}/include/emp-tool/files)
 if(EMP-TOOL_FOUND)
 	set(EMP-TOOL_LIBRARIES ${EMP-TOOL_LIBRARY})
 	set(EMP-TOOL_INCLUDE_DIRS ${EMP-TOOL_INCLUDE_DIR})
