629bd0dc47fc9fc55dc86f3db9bafc1f9371cff3
Peter Rindal <rindalp@oregonstate.edu>
2017-06-06 10:44:41 -0700

    cleanup and added back original networking

--- emp-tool-config.cmake
+++ emp-tool-config.cmake
@@ -1,25 +1,18 @@
 
 
-if(MSVC AND EXISTS ${CMAKE_SOURCE_DIR}/../emp-tool)
-	# on MSVC, try to use local source tree...
-	set(EMP-TOOL_LIBRARIES "${CMAKE_SOURCE_DIR}/../emp-tool/bin/\$(Configuration)/emp-tool.lib")
-	set(EMP-TOOL_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/../;${CMAKE_SOURCE_DIR}/../emp-tool)
-	set(EMP-TOOL_FOUND true)
-else()
-	# find the location of where this file was installed. We expect includes/libs to be next to it
-	find_path(EMP-TOOL_INSTALL_DIR NAMES cmake/emp-tool-config.cmake)
+
+# find the location of where this file was installed. We expect includes/libs to be next to it
+find_path(EMP-TOOL_INSTALL_DIR NAMES cmake/emp-tool-config.cmake)
 
 
-	find_library(EMP-TOOL_LIBRARY NAMES emp-tool)
+find_library(EMP-TOOL_LIBRARY NAMES emp-tool)
 
-	include(FindPackageHandleStandardArgs)
-	find_package_handle_standard_args(EMP-TOOL       DEFAULT_MSG EMP-TOOL_INSTALL_DIR EMP-TOOL_LIBRARY)
-	#find_package_handle_standard_args(EMP-TOOL_DEBUG DEFAULT_MSG EMP-TOOL_INSTALL_DIR EMP-TOOL_DEBUG_LIBRARY)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(EMP-TOOL       DEFAULT_MSG EMP-TOOL_INSTALL_DIR EMP-TOOL_LIBRARY)
+#find_package_handle_standard_args(EMP-TOOL_DEBUG DEFAULT_MSG EMP-TOOL_INSTALL_DIR EMP-TOOL_DEBUG_LIBRARY)
 
-	add_definitions(-DEMP_CIRCUIT_PATH=${EMP-TOOL_INSTALL_DIR}/include/emp-tool/files/)
-	if(EMP-TOOL_FOUND)
-		set(EMP-TOOL_LIBRARIES ${EMP-TOOL_LIBRARY})
-		set(EMP-TOOL_INCLUDE_DIRS ${EMP-TOOL_INSTALL_DIR}/include/)
-	endif()
-
+add_definitions(-DEMP_CIRCUIT_PATH=${EMP-TOOL_INSTALL_DIR}/include/emp-tool/circuits/files/)
+if(EMP-TOOL_FOUND)
+	set(EMP-TOOL_LIBRARIES ${EMP-TOOL_LIBRARY})
+	set(EMP-TOOL_INCLUDE_DIRS ${EMP-TOOL_INSTALL_DIR}/include/)
 endif()
--- io/net_io_channel.h
+++ io/net_io_channel.h
@@ -2,15 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <unistd.h>
-//#include <arpa/inet.h>
-//#include <netinet/tcp.h>
-//#include <netinet/in.h>
-//#include <sys/socket.h>
+
 //#include <cryptoTools/Network/Channel.h>
 //#include <cryptoTools/Network/Endpoint.h>
 //#include <cryptoTools/Network/IOService.h>
+
+#ifdef _MSC_VER 
+#define USE_ASIO
+#endif
+
+#ifdef USE_ASIO 
 #include <boost/asio.hpp>
+#else
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#endif
 
 #include <sys/types.h>
 
@@ -21,175 +30,161 @@ using namespace std;
 #ifndef NETWORK_IO_CHANNEL
 #define NETWORK_IO_CHANNEL
 /** @addtogroup IO
-	@{
+    @{
   */
 
-//class __networking
-//{
-//public:
-//	osuCrypto::IOService mIoService;
-//	std::list<osuCrypto::Endpoint> mEndpoints;
-//	std::mutex mMtx;
-//
-//	~__networking()
-//	{
-//		for (auto& e : mEndpoints)
-//			e.stop();
-//		mEndpoints.clear();
-//
-//		mIoService.stop();
-//	}
-//};
-//
-//extern  __networking gNetworking;
+
 
 extern  boost::asio::io_service emp_io_service;
 
 class NetIO : public IOChannel<NetIO> {
 public:
-	bool is_server;
-    boost::asio::ip::tcp::socket mSock;
+    bool is_server;
 
-	//int mysocket = -1;
-	//int consocket = -1;
-	//FILE * stream = nullptr;
-	//char * buffer = nullptr;
-	bool has_sent = false;
-	string addr;
-	int port;
+#ifdef USE_ASIO 
+    boost::asio::ip::tcp::socket mSock;
+#else
+    int mysocket = -1;
+    int consocket = -1;
+    FILE * stream = nullptr;
+    char * buffer = nullptr;
+#endif 
+
+    bool has_sent = false;
+    string addr;
+    int port;
 #ifdef COUNT_IO
-	uint64_t counter = 0;
+    uint64_t counter = 0;
 #endif
-	NetIO(const char * address, int port, bool quiet = false)
+
+    NetIO(const char * address, int port, bool quiet = false)
+#ifdef USE_ASIO 
         :mSock(emp_io_service)
+#endif
     {
+
+#ifdef USE_ASIO
         using namespace boost::asio::ip;
-		this->port = port;
-		is_server = (address == nullptr);
-		if (is_server) {
-			// wildcard address
-			//addr = "0.0.0.0";
+        this->port = port;
+        is_server = (address == nullptr);
+        if (is_server) {
+            // wildcard address
+            //addr = "0.0.0.0";
 
             tcp::acceptor acceptor(emp_io_service, tcp::endpoint(tcp::v4(), port));
             acceptor.accept(mSock);
 
-		}
-		else
-		{
-			addr = address;
+        }
+        else
+        {
+            addr = address;
             tcp::resolver resolver(emp_io_service);
             tcp::resolver::query query(addr, std::to_string(port));
             tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-            boost::asio::connect(mSock, endpoint_iterator);
-		}
-        boost::asio::ip::tcp::no_delay option(true);
-        mSock.set_option(option);
-		//{
+
+            boost::system::error_code ec;
+            boost::asio::connect(mSock, endpoint_iterator, ec);
+            while (ec) {
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                boost::asio::connect(mSock, endpoint_iterator, ec);
+            }
+        }
 
 
-			////std::lock_guard<std::mutex> lock(gNetworking.mMtx);
-			//bool set = 0;
-			//for (auto& ep : gNetworking.mEndpoints)
-			//{
-			//	if (ep.IP() == addr && ep.port() == port)
-			//	{
-			//		chan = ep.addChannel("c");
-			//		set = 1;
-			//	}
-			//}
+#else
+        if (address == nullptr) {
+            struct sockaddr_in dest;
+            struct sockaddr_in serv;
+            socklen_t socksize = sizeof(struct sockaddr_in);
+            memset(&serv, 0, sizeof(serv));
+            serv.sin_family = AF_INET;
+            serv.sin_addr.s_addr = htonl(INADDR_ANY); /* set our address to any interface */
+            serv.sin_port = htons(port);           /* set the server port number */
+            mysocket = socket(AF_INET, SOCK_STREAM, 0);
+            int reuse = 1;
+            setsockopt(mysocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
+            bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+            listen(mysocket, 1);
+            consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+        }
+        else {
+            addr = string(address);
+            struct sockaddr_in dest;
+            consocket = socket(AF_INET, SOCK_STREAM, 0);
+            memset(&dest, 0, sizeof(dest));
+            dest.sin_family = AF_INET;
+            dest.sin_addr.s_addr = inet_addr(address);
+            dest.sin_port = htons(port);
+            while (connect(consocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == -1) {
+                usleep(1000);
+            }
+        }
 
-			//if (!set)
-			//{
-			//	gNetworking.mEndpoints.emplace_back(gNetworking.mIoService, addr, port, is_server ? osuCrypto::EpMode::Server : osuCrypto::EpMode::Client, "c");
-			//	auto& ep = gNetworking.mEndpoints.back();
-			//	chan = ep.addChannel("c");
-			//}
-		//}
+        stream = fdopen(consocket, "wb+");
+        buffer = new char[NETWORK_BUFFER_SIZE];
+        memset(buffer, 0, NETWORK_BUFFER_SIZE);
+        setvbuf(stream, buffer, _IOFBF, NETWORK_BUFFER_SIZE);
 
-
-
-		//chan.waitForConnection();
-
-		if (!quiet)
-			cout << "connected" << endl;
-		//return;
-
-		//struct sockaddr_in dest;
-		//struct sockaddr_in serv;
-		//socklen_t socksize = sizeof(struct sockaddr_in);
-		//memset(&serv, 0, sizeof(serv));
-		//serv.sin_family = AF_INET;
-		//serv.sin_addr.s_addr = htonl(INADDR_ANY); /* set our address to any interface */
-		//serv.sin_port = htons(port);           /* set the server port number */    
-		//mysocket = socket(AF_INET, SOCK_STREAM, 0);
-		//int reuse = 1;
-		//setsockopt(mysocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
-		//bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr));
-		//listen(mysocket, 1);
-		//consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
-		//}
-		//else {
-
-
-		//	//addr = string(address);
-		//	//struct sockaddr_in dest; 
-		//	//consocket = socket(AF_INET, SOCK_STREAM, 0);
-		//	//memset(&dest, 0, sizeof(dest));
-		//	//dest.sin_family = AF_INET;
-		//	//dest.sin_addr.s_addr = inet_addr(address);
-		//	//dest.sin_port = htons(port);
-		//	//while(connect(consocket, (struct sockaddr *)&dest, sizeof(struct sockaddr)) == -1) {
-		//	//	usleep(1000);
-		//	//}
-		//}
-//		set_nodelay();
-		//if(!quiet)
-		//	cout << "connected"<<endl;
-		//stream = fdopen(consocket, "wb+");
-		//buffer = new char[NETWORK_BUFFER_SIZE];
-		//memset(buffer, 0, NETWORK_BUFFER_SIZE);
-		//setvbuf(stream, buffer, _IOFBF, NETWORK_BUFFER_SIZE);
-	}
-
-	~NetIO() {
-		//fflush(stream);
-		//close(consocket);
-		//delete[] buffer;
-
-		//auto & ep = chan.getEndpoint();
-		//chan.close();
-	}
-	void sync() {
-		int tmp = 0;
-		if (is_server) {
-			send_data(&tmp, 1);
-			recv_data(&tmp, 1);
-		}
-		else {
-			recv_data(&tmp, 1);
-			send_data(&tmp, 1);
-			flush();
-		}
-	}
-	void set_nodelay() {
-		//const int one=1;
-		//setsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&one,sizeof(one));
-	}
-
-	void set_delay() {
-		//const int zero = 0;
-		//setsockopt(consocket,IPPROTO_TCP,TCP_NODELAY,&zero,sizeof(zero));
-	}
-
-	void flush() {
-		//fflush(stream);
-	}
-
-	void send_data_impl(const void * data, int len) {
-#ifdef COUNT_IO
-		counter += len;
 #endif
 
+        if (!quiet)
+            cout << "connected" << endl;
+    }
+
+    ~NetIO() {
+#ifndef USE_ASIO
+        fflush(stream);
+        close(consocket);
+        delete[] buffer;
+
+        auto & ep = chan.getEndpoint();
+        chan.close();
+#endif
+    }
+    void sync() {
+        int tmp = 0;
+        if (is_server) {
+            send_data(&tmp, 1);
+            recv_data(&tmp, 1);
+        }
+        else {
+            recv_data(&tmp, 1);
+            send_data(&tmp, 1);
+            flush();
+        }
+    }
+    void set_nodelay() {
+#ifdef USE_ASIO
+        boost::asio::ip::tcp::no_delay option(true);
+        mSock.set_option(option);
+#else
+        const int one = 1;
+        setsockopt(consocket, IPPROTO_TCP, TCP_NODELAY, &one, sizeof(one));
+#endif
+    }
+
+    void set_delay() {
+#ifdef USE_ASIO
+        boost::asio::ip::tcp::no_delay option(false);
+        mSock.set_option(option);
+#else
+        const int zero = 0;
+        setsockopt(consocket, IPPROTO_TCP, TCP_NODELAY, &zero, sizeof(zero));
+#endif
+    }
+
+    void flush() {
+#ifndef USE_ASIO
+        fflush(stream);
+#endif
+    }
+
+    void send_data_impl(const void * data, int len) {
+#ifdef COUNT_IO
+        counter += len;
+#endif
+
+#ifdef USE_ASIO
         boost::system::error_code ec;
         boost::asio::write(mSock, boost::asio::buffer(data, len), ec);
 
@@ -198,21 +193,21 @@ public:
             std::cout << "network error" << std::endl << ec.message() << std::endl;
             std::terminate();
         }
-		//chan.asyncSendCopy(data, len);
+#else
+        int sent = 0;
+        while (sent < len) {
+            int res = fwrite(sent + (char*)data, 1, len - sent, stream);
+            if (res >= 0)
+                sent += res;
+            else
+                fprintf(stderr, "error: net_send_data %d\n", res);
+        }
+#endif
+        has_sent = true;
+    }
 
-		//int sent = 0;
-		//while (sent < len) {
-		//	int res = fwrite(sent + (char*)data, 1, len - sent, stream);
-		//	if (res >= 0)
-		//		sent += res;
-		//	else
-		//		fprintf(stderr, "error: net_send_data %d\n", res);
-		//}
-		has_sent = true;
-	}
-
-	void recv_data_impl(void  * data, int len) {
-
+    void recv_data_impl(void  * data, int len) {
+#ifdef USE_ASIO
         boost::system::error_code ec;
         boost::asio::read(mSock, boost::asio::buffer(data, len), ec);
 
@@ -221,19 +216,20 @@ public:
             std::cout << "network error" << std::endl << ec.message() << std::endl;
             std::terminate();
         }
-		//chan.recv(data, len);
-		//if (has_sent)
-		//	fflush(stream);
-		//has_sent = false;
-		//int sent = 0;
-		//while (sent < len) {
-		//	int res = fread(sent + (char*)data, 1, len - sent, stream);
-		//	if (res >= 0)
-		//		sent += res;
-		//	else
-		//		fprintf(stderr, "error: net_send_data %d\n", res);
-		//}
-	}
+#else
+        if (has_sent)
+            fflush(stream);
+        has_sent = false;
+        int sent = 0;
+        while (sent < len) {
+            int res = fread(sent + (char*)data, 1, len - sent, stream);
+            if (res >= 0)
+                sent += res;
+            else
+                fprintf(stderr, "error: net_send_data %d\n", res);
+        }
+#endif
+    }
 };
 
 
