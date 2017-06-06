b160e2a8466f44f6fd0f217c2b78aa9ca758590a
Peter Rindal <rindalp@oregonstate.edu>
2017-06-05 21:05:20 -0700

    prive free update

--- gc/privacy_free_gen.h
+++ gc/privacy_free_gen.h
@@ -8,21 +8,21 @@
 #include "emp-tool/utils/hash.h"
 #include "emp-tool/garble/garble_gate_privacy_free.h"
 #include <iostream>
-
-template<typename T>
-bool privacy_free_gen_is_public(GarbleCircuit* gc, const block & b, EmpParty party);
-
-template<typename T>
-block privacy_free_gen_public_label(GarbleCircuit* gc, bool b);
-
-template<typename T>
-block privacy_free_gen_and(GarbleCircuit* gc, const block&a, const block&b);
-
-template<typename T>
-block privacy_free_gen_xor(GarbleCircuit*gc, const block&a, const block&b);
-
-template<typename T>
-block privacy_free_gen_not(GarbleCircuit*gc, const block&a);
+//
+//template<typename T>
+//bool privacy_free_gen_is_public(GarbleCircuit* gc, const block & b, EmpParty party);
+//
+//template<typename T>
+//block privacy_free_gen_public_label(GarbleCircuit* gc, bool b);
+//
+//template<typename T>
+//block privacy_free_gen_and(GarbleCircuit* gc, const block&a, const block&b);
+//
+//template<typename T>
+//block privacy_free_gen_xor(GarbleCircuit*gc, const block&a, const block&b);
+//
+//template<typename T>
+//block privacy_free_gen_not(GarbleCircuit*gc, const block&a);
 
 template<typename T>
 class PrivacyFreeGen: public GarbleCircuit{ public:
@@ -35,13 +35,13 @@ class PrivacyFreeGen: public GarbleCircuit{ public:
 		block a;
 		tmp.random_block(&a, 1);
 		set_delta(a);
-		is_public_ptr = &privacy_free_gen_is_public<T>;
-		public_label_ptr = &privacy_free_gen_public_label<T>;
-		gc_and_ptr = &privacy_free_gen_and<T>;
-		gc_xor_ptr = &privacy_free_gen_xor<T>;
-		gc_not_ptr = &privacy_free_gen_not<T>;
+		//is_public_ptr = &privacy_free_gen_is_public<T>;
+		//public_label_ptr = &privacy_free_gen_public_label<T>;
+		//gc_and_ptr = &privacy_free_gen_and<T>;
+		//gc_xor_ptr = &privacy_free_gen_xor<T>;
+		//gc_not_ptr = &privacy_free_gen_not<T>;
 	}
-	bool is_public_impl(const block & b, EmpParty party) {
+	bool is_public(const block & b, EmpParty party) override {
 		return false;
 	}
 	bool isDelta(const block & b) {
@@ -55,21 +55,21 @@ class PrivacyFreeGen: public GarbleCircuit{ public:
 		*((char *) &constant[1]) |= 0x01;
 		constant[1] = xorBlocks(constant[1], delta);
 	}
-	block public_label_impl(bool b) {
+	block public_label(bool b) override {
 		return constant[b];
 	}
-	block gen_and(const block& a, const block& b) {
+	block gc_and(const block& a, const block& b) override {
 		block out[2], table[2];
 		garble_gate_garble_privacy_free(GARBLE_GATE_AND, a, xorBlocks(a,delta), b, xorBlocks(b,delta), 
 				&out[0], &out[1], delta, table, gid++, prp.aes);
 		io->send_block(table, 1);
 		return out[0];
 	}
-	block gen_xor(const block&a, const block& b) {
+	block gc_xor(const block&a, const block& b) override {
 		return xorBlocks(a, b);
 	}
-	block gen_not(const block& a) {
-		return gen_xor(a, public_label_impl(true));
+	block gc_not(const block& a) override {
+		return gen_xor(a, public_label(true));
 	}
 	void privacy_free_to_xor(const block* new_b0,const block * b0, const block * b1, int length){
 		block h[2];
@@ -83,25 +83,26 @@ class PrivacyFreeGen: public GarbleCircuit{ public:
 		}
 	}
 };
-template<typename T>
-bool privacy_free_gen_is_public(GarbleCircuit* gc, const block & b, EmpParty party) {
-	return ((PrivacyFreeGen<T>*)gc)->is_public_impl(b, party);
-}
-template<typename T>
-block privacy_free_gen_public_label(GarbleCircuit* gc, bool b) {
-	return ((PrivacyFreeGen<T>*)gc)->public_label_impl(b);
-}
-template<typename T>
-block privacy_free_gen_and(GarbleCircuit* gc, const block&a, const block&b) {
-	return ((PrivacyFreeGen<T>*)gc)->gen_and(a, b);
-}
-template<typename T>
-block privacy_free_gen_xor(GarbleCircuit*gc, const block&a, const block&b) {
-	return ((PrivacyFreeGen<T>*)gc)->gen_xor(a, b);
-}
-
-template<typename T>
-block privacy_free_gen_not(GarbleCircuit*gc, const block&a) {
-	return ((PrivacyFreeGen<T>*)gc)->gen_not(a);
-}
+//
+//template<typename T>
+//bool privacy_free_gen_is_public(GarbleCircuit* gc, const block & b, EmpParty party) {
+//	return ((PrivacyFreeGen<T>*)gc)->is_public_impl(b, party);
+//}
+//template<typename T>
+//block privacy_free_gen_public_label(GarbleCircuit* gc, bool b) {
+//	return ((PrivacyFreeGen<T>*)gc)->public_label_impl(b);
+//}
+//template<typename T>
+//block privacy_free_gen_and(GarbleCircuit* gc, const block&a, const block&b) {
+//	return ((PrivacyFreeGen<T>*)gc)->gen_and(a, b);
+//}
+//template<typename T>
+//block privacy_free_gen_xor(GarbleCircuit*gc, const block&a, const block&b) {
+//	return ((PrivacyFreeGen<T>*)gc)->gen_xor(a, b);
+//}
+//
+//template<typename T>
+//block privacy_free_gen_not(GarbleCircuit*gc, const block&a) {
+//	return ((PrivacyFreeGen<T>*)gc)->gen_not(a);
+//}
 #endif// PRIVACY_FREE_GEN_H__
