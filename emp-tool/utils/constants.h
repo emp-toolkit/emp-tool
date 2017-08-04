72c47f11401ef7f73c387ce246961adc9089ba67
Xiao Wang <wangxiao1254@gmail.com>
2017-08-04 10:35:35 -0400

    add prefix for xor

--- emp-tool/utils/constants.h
+++ emp-tool/utils/constants.h
@@ -8,7 +8,7 @@ const static int FILE_BUFFER_SIZE = 1024*16;
 const static int CHECK_BUFFER_SIZE = 1024*8;
 //#define THREADING
 
-const static int XOR = -1;
+const static int EMP_XOR = -1;
 const static int PUBLIC = 0;
 const static int ALICE = 1;
 const static int BOB = 2;
