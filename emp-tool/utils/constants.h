9e02605e94fb363f5f3a552151f8b9a2455414e4
Xiao Wang <wangxiao1254@gmail.com>
2017-08-17 02:11:17 -0400

    adding windows support for NetIO

--- emp-tool/utils/constants.h
+++ emp-tool/utils/constants.h
@@ -15,7 +15,7 @@ const static int ALICE = 1;
 const static int BOB = 2;
 
 #if defined(unix) || defined(__unix__) || defined(__unix)
-#define EMP_UNIX2
+#define EMP_UNIX
 #endif
 
 #endif// CONFIG_H__
