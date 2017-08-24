ce35c106505f5a754f5f1e2bc4a87ddbdd2021ef
Xiao Wang <wangxiao1254@gmail.com>
2017-08-24 09:59:59 -0700

    add threading option

--- emp-tool/emp-tool.cpp
+++ emp-tool/emp-tool.cpp
@@ -1,5 +1,10 @@
 #include "emp-tool/execution/circuit_execution.h"
 #include "emp-tool/execution/protocol_execution.h"
 
+#ifndef THREADING
 emp::ProtocolExecution* emp::ProtocolExecution::prot_exec = nullptr;
 emp::CircuitExecution* emp::CircuitExecution::circ_exec = nullptr;
+#else
+__thread emp::ProtocolExecution* emp::ProtocolExecution::prot_exec = nullptr;
+__thread emp::CircuitExecution* emp::CircuitExecution::circ_exec = nullptr;
+#endif
--- emp-tool/utils/constants.h
+++ emp-tool/utils/constants.h
@@ -6,8 +6,6 @@ const static int HASH_BUFFER_SIZE = 1024*8;
 const static int NETWORK_BUFFER_SIZE = 1024*16;//Should change depending on the network
 const static int FILE_BUFFER_SIZE = 1024*16;
 const static int CHECK_BUFFER_SIZE = 1024*8;
-//#define THREADING
-//#define EMP_USE_RANDOM_DEVICE
 
 const static int XOR = -1;
 const static int PUBLIC = 0;
