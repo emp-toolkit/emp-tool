d1416389b3b3eac0cccb4b2566ccd17a9a4cf3d5
weikeng <w.k@berkeley.edu>
2017-10-28 19:00:21 -0700

    Set Mac (and maybe iPhone) to unix

--- emp-tool/utils/constants.h
+++ emp-tool/utils/constants.h
@@ -12,7 +12,7 @@ const static int PUBLIC = 0;
 const static int ALICE = 1;
 const static int BOB = 2;
 
-#if defined(unix) || defined(__unix__) || defined(__unix)
+#if defined(unix) || defined(__unix__) || defined(__unix) || defined(__APPLE__)
 #define UNIX_PLATFORM
 #endif
 }
