082b70b5204adc589cfd0c4523ea333d20add9c6
Xiao Wang <wangxiao1254@gmail.com>
2020-05-20 21:35:40 -0500

    update cmake

--- cmake/emp-tool-config.cmake
+++ cmake/emp-tool-config.cmake
@@ -4,7 +4,7 @@ find_path(EMP-TOOL_INCLUDE_DIR NAMES emp-tool/emp-tool.h)
 find_library(EMP-TOOL_LIBRARY NAMES emp-tool)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(EMP-TOOL DEFAULT_MSG EMP-TOOL_INCLUDE_DIR EMP-TOOL_LIBRARY)
+find_package_handle_standard_args(emp-tool DEFAULT_MSG EMP-TOOL_INCLUDE_DIR EMP-TOOL_LIBRARY)
 
 add_definitions(-DEMP_CIRCUIT_PATH=${EMP-TOOL_INCLUDE_DIR}/emp-tool/circuits/files/)
 if(EMP-TOOL_FOUND)
