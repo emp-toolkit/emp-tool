b95f935a75134f2c0c9c3fb9763c126c14b80bfd
dymil <30931139+dymil@users.noreply.github.com>
2020-08-03 13:39:32 -0400

    Compare to 1 rather than evaluating boolean

--- cmake/source_of_randomness.cmake
+++ cmake/source_of_randomness.cmake
@@ -8,9 +8,10 @@ unset(RDSEED_RUN_RESULT CACHE)
 set(CMAKE_REQUIRED_FLAGS "-mrdseed") # in case CMAKE_C_FLAGS doesn't have it, ex. if add_compile_options is used instead
 check_c_source_runs("#include <x86intrin.h>\nint main(){\nunsigned long long r;\n_rdseed64_step(&r);\nreturn 0;\n}\n" RDSEED_RUN_RESULT)
 
-IF(NOT ${RDSEED_RUN_RESULT})
+string(COMPARE EQUAL "${RDSEED_RUN_RESULT}" "1" RDSEED_RUN_SUCCESS)
+IF(NOT ${RDSEED_RUN_SUCCESS})
 	set(USE_RANDOM_DEVICE ON)
-ENDIF(NOT ${RDSEED_RUN_RESULT})
+ENDIF(NOT ${RDSEED_RUN_SUCCESS})
 
 IF(${USE_RANDOM_DEVICE})
 	ADD_DEFINITIONS(-DEMP_USE_RANDOM_DEVICE)
