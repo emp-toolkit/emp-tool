931ac8e3cfe10e52ab0173f34715811b0d45349a
Xiao Wang <wangxiao1254@gmail.com>
2020-11-06 22:47:55 -0600

    update network counter from int to int64

--- emp-tool/io/io_channel.h
+++ emp-tool/io/io_channel.h
@@ -8,7 +8,7 @@
 namespace emp {
 template<typename T> 
 class IOChannel { public:
-	int counter = 0;
+	uint64_t counter = 0;
 	void send_data(const void * data, int nbyte) {
 		counter +=nbyte;
 		derived().send_data_internal(data, nbyte);
