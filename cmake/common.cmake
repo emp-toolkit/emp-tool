1682b975101163c86bd41cdf466ea0cf3de28191
Xiao Wang <wangxiao@Xiaos-MacBook-Pro.local>
2020-12-02 23:55:13 -0600

    update for M1 chip

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -33,12 +33,12 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
 endif()
 
 #Compilation flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread -Wall -march=native -funroll-loops")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread -Wall -funroll-loops")
 message("${Blue}-- Platform: ${CMAKE_SYSTEM_PROCESSOR}${ColourReset}")
-IF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "aarch64")
-ELSE(${CMAKE_SYSTEM_PROCESSOR} MATCHES "aarch64")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -maes -mrdseed")
-ENDIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "aarch64")
+IF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "(aarch64)|(arm64)")
+ELSE(${CMAKE_SYSTEM_PROCESSOR} MATCHES "(aarch64)|(arm64)")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=native -maes -mrdseed")
+ENDIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "(aarch64)|(arm64)" )
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS} -std=c++11")
 
 ## Build type
