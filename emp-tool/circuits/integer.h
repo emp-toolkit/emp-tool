95ef8a560656791cf3ccc0d59c3446c3dd594f34
Fan Zhang <bl4ck5unxx@gmail.com>
2021-05-17 14:18:52 -0400

    Update integer.h

--- emp-tool/circuits/integer.h
+++ emp-tool/circuits/integer.h
@@ -16,7 +16,7 @@ class Integer : public Swappable<Integer>, public Comparable<Integer> { public:
 	vector<Bit> bits;
 	Integer(){
 	}
-	Integer(const vector<Bit> bits): bits(bits) {
+	Integer(const vector<Bit>& bits): bits(bits) {
 	}
 	Integer(int length, int64_t input, int party = PUBLIC);
 
