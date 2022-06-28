20978b10d58070ae67bd729cce427b81dc0f355b
Mohammed Das <das.mohammed@gmail.com>
2022-06-27 21:18:23 -0700

    Remove c++11 as compilers have beyond it by default

--- cmake/emp-base.cmake
+++ cmake/emp-base.cmake
@@ -49,7 +49,9 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a+simd+crypto+crc")
 ELSE(${CMAKE_SYSTEM_PROCESSOR} MATCHES "(aarch64)|(arm64)")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=native -maes -mrdseed")
 ENDIF(${CMAKE_SYSTEM_PROCESSOR} MATCHES "(aarch64)|(arm64)" )
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS} -std=c++11")
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_C_FLAGS}")
+
 
 ## Build type
 if(NOT CMAKE_BUILD_TYPE)
