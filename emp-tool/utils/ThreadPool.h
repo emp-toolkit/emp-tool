88f722f64e7ccb2546afe8b4d7a3f043ed2f6b42
Yibin Yang <yyang811@gatech.edu>
2024-10-21 17:26:03 -0400

    [upgrade] ThreadPool now supports C++20 (#195)

--- emp-tool/utils/ThreadPool.h
+++ emp-tool/utils/ThreadPool.h
@@ -39,8 +39,13 @@ class ThreadPool {
 public:
     ThreadPool(size_t);
     template<class F, class... Args>
+#if __cplusplus >= 202002L
+    auto enqueue(F&& f, Args&&... args) 
+        -> std::future<typename std::invoke_result<F,Args...>::type>;
+#else
     auto enqueue(F&& f, Args&&... args) 
         -> std::future<typename std::result_of<F(Args...)>::type>;
+#endif	
     ~ThreadPool();
 	int size() const;
 private:
@@ -87,11 +92,19 @@ inline ThreadPool::ThreadPool(size_t threads)
 }
 
 // add new work item to the pool
+#if __cplusplus >= 202002L
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) 
+    -> std::future<typename std::invoke_result<F,Args...>::type>
+{
+    using return_type = typename std::invoke_result<F,Args...>::type;
+#else
 template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
     using return_type = typename std::result_of<F(Args...)>::type;
+#endif
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
